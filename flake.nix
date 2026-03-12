1acd5210b57b3d7164f21488c6a79c9a5210338c
ptem <emily.anible@gmail.com>
2026-03-12 03:15:12 -0400

    home-manager tracking master

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     agenix.url = "github:ryantm/agenix";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
