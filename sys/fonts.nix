83092ec6a161a7579a672fdda7d42adc3a15d81c
ptem <emily.anible@gmail.com>
2026-03-15 09:56:32 -0400

    extracted fonts

---
+++ sys/fonts.nix
@@ -0,0 +1,78 @@
+# sys/fonts.nix
+{ ... }:
+
+{
+  environment.sessionVariables = {
+    # Freetype engine
+    FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0 truetype:interpreter-version=40";
+  };
+
+  # fonts
+  console.font = "sun12x22";
+
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = false;
+
+    packages = with pkgs; [
+      # Text (Sans + Serif + Mono)
+      ibm-plex
+      noto-fonts
+      geist-font
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code
+
+      # Symbols
+      material-design-icons
+      nerd-fonts.symbols-only
+
+      # Emoji
+      twitter-color-emoji
+      noto-fonts-color-emoji
+
+      # Arial/TNR compatibility
+      liberation_ttf
+    ];
+
+    fontconfig = {
+      enable = true;
+      antialias = true;
+
+      # align font vectors to pixel grid slightly
+      hinting = {
+        enable = true;
+        style = "slight";
+        autohint = true;
+      };
+
+      # subpixel rendering
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
+      };
+
+      # generally nerd-fonts.font-name -> "FontName Nerd Font"
+      # Each defaultFonts option takes a list
+      # First is highest prio, subsequent are fallbacks
+      defaultFonts = {
+        sansSerif = [
+          "Geist"
+          "Noto Sans Serif"
+        ];
+        serif = [
+          "IBM Plex Serif"
+          "Noto Serif"
+        ];
+        monospace = [
+          "IBM Plex Mono Text"
+          "JetBrainsMono Nerd Font"
+        ];
+        emoji = [
+          "Twitter Color Emoji"
+          "Noto Color Emoji"
+        ];
+      };
+    };
+  };
+
+}
--- sys/plasma.nix
+++ sys/plasma.nix
@@ -35,8 +35,6 @@
     KWIN_DRM_PREFER_COLOR_DEPTH = "30";
     AMD_DEBUG = "wsi_force_bgra8_unorm=0";
 
-    # Freetype Engine
-    FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0 truetype:interpreter-version=40";
     QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
   };
 
@@ -49,72 +47,4 @@
     kate
   ];
 
-  # fonts
-  console.font = "sun12x22";
-
-  fonts = {
-    fontDir.enable = true;
-    enableDefaultPackages = false;
-
-    packages = with pkgs; [
-      # Text (Sans + Serif + Mono)
-      ibm-plex
-      noto-fonts
-      geist-font
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.fira-code
-
-      # Symbols
-      material-design-icons
-      nerd-fonts.symbols-only
-
-      # Emoji
-      twitter-color-emoji
-      noto-fonts-color-emoji
-
-      # Arial/TNR compatibility
-      liberation_ttf
-    ];
-
-    fontconfig = {
-      enable = true;
-      antialias = true;
-
-      # align font vectors to pixel grid slightly
-      hinting = {
-        enable = true;
-        style = "slight";
-        autohint = true;
-      };
-
-      # subpixel rendering
-      subpixel = {
-        rgba = "rgb";
-        lcdfilter = "default";
-      };
-
-      # generally nerd-fonts.font-name -> "FontName Nerd Font"
-      # Each defaultFonts option takes a list
-      # First is highest prio, subsequent are fallbacks
-      defaultFonts = {
-        sansSerif = [
-          "Geist"
-          "Noto Sans Serif"
-        ];
-        serif = [
-          "IBM Plex Serif"
-          "Noto Serif"
-        ];
-        monospace = [
-          "IBM Plex Mono Text"
-          "JetBrainsMono Nerd Font"
-        ];
-        emoji = [
-          "Twitter Color Emoji"
-          "Noto Color Emoji"
-        ];
-      };
-    };
-
-  };
 }
