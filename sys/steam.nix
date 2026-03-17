ae1f9ea44d26a733d307649e68a2a920a6eafb89
ptem <13301888+ptem@users.noreply.github.com>
2026-03-17 18:26:14 -0400

    gamescope stuff. should fix deadlock ghosting issues, surely

--- sys/steam.nix
+++ sys/steam.nix
@@ -2,11 +2,30 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    gamescope
+    mangohud
+  ];
+
   # Steam requires system-level installation for reasons
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     extraCompatPackages = with pkgs; [ proton-ge-bin ];
+
+    gamescopeSession = {
+      enable = true;
+
+      args = [ "mangoapp" ];
+    };
+
+    package = pkgs.steam.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          libkrb5
+          keyutils
+        ];
+    };
   };
 }
