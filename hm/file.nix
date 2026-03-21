aa35ea21b64239b47ab8ea42aa15c58d13666e8c
ptem <13301888+ptem@users.noreply.github.com>
2026-03-21 06:21:00 -0400

    ranger config

--- hm/file.nix
+++ hm/file.nix
@@ -6,34 +6,47 @@
   programs.ranger = {
     enable = true;
 
-    # plugins = [
-    #   {
-    #     name = "zoxide";
-    #     src = builtins.fetchGit {
-    #       url = "https://github.com/jchook/ranger-zoxide.git";
-    #       rev = "363df97af34c96ea873c5b13b035413f56b12ead";
-    #     };
-    #   }
-    # ];
+    plugins = [
+      {
+        name = "ranger-devicons2";
+        src = pkgs.fetchFromGitHub {
+          owner = "cdump";
+          repo = "ranger-devicons2";
+          rev = "94bdcc19218681debb252475fd9d11cfd274d9b1";
+          hash = "sha256-aJCIoDfzmOnzMLlbOe+dy6129n5Dc4OrefhHnPsgI8k=";
+        };
+      }
+    ];
 
     aliases = {
       e = "edit";
+      dots = "cd ~/.dotfiles";
     };
 
     settings = {
-      # column_ratios = "1,3,3";
-      # confirm_on_delete = "never";
-      # scroll_offset = 8;
+      column_ratios = "1,3,3";
+      confirm_on_delete = "always";
+      vcs_aware = true;
+
       unicode_ellipsis = true;
       preview_images = true;
+      preview_max_size = 10485760;
+      draw_borders = "both";
       preview_images_method = "kitty";
+      use_preview_script = true;
+      preview_script = "~/.config/ranger/scope.sh";
+      wrap_plaintext_previews = true;
+      show_hidden = true;
     };
 
     extraPackages = with pkgs; [
-      ueberzugpp
       ffmpegthumbnailer
       file
       libarchive
+      lynx
+      haskellPackages.pdftotext
+      haskellPackages.epub
+      # bat -- already have
     ];
 
     # rifle = [
@@ -41,40 +54,44 @@
     #   # rifle.*.command, .condition
     # ];
 
+    extraConfig = ''
+      default_linemode devicons2
+    '';
   };
 
-  # https://github.com/jarun/nnn
-  programs.nnn = {
-    enable = false;
+  # preview script
+  xdg.configFile."ranger/scope.sh" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+      # arguments passed by ranger
+      path="$1"         # path of the selected file
+      width="$2"        # preview pane width
+      height="$3"       # preview pane height
+      image_enable="$5"
 
-    # package = pkgs.nnn.override { withNerdIcons = true; };
+      extension=$(echo "''${path##*.}" | tr '[:upper:]' '[:lower:]')
 
-    # bookmarks = {
-    #   d = "~/.dotfiles";
-    #   D = "~/Downloads";
-    # };
+      case "$extension" in
+          md|markdown)
+              bat --color=always "$path" && exit 5
+              exit 1;;
+      esac
 
-    # extraPackages = with pkgs; [
-    #   ffmpegthumbnailer
-    #   mediainfo
-    #   sxiv
-    # ];
+      mimetype=$(file --mime-type -Lb "$path")
 
-    # plugins = {
-    #   src =
-    #     (pkgs.fetchFromGitHub {
-    #       owner = "jarun";
-    #       repo = "nnn";
-    #       rev = "v5.2";
-    #       sha256 = "sha256-u+88aDHfOZ6bSkg6ahS6eNZWj2QCwJXKW+8nHR99kic=";
-    #     })
-    #     + "/plugins";
+      case "$mimetype" in
+        image/*)
+          if [[ "$image_enable" == "True" ]]; then
+            exit 7
+          fi
+          exit 1;;
+        text/* | application/json | */xml)
+          bat --color=always --style=plain "$path" && exit 5
+          exit 1;;
+      esac
 
-    #   mappings = {
-    #     v = "imgview";
-    #     p = "preview-tui";
-    #   };
-    # };
-
+      exit 1
+    '';
   };
 }
