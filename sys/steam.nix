b9bc6000cfa57dd130708c2ab1dc1eed50fcdd98
ptem <13301888+ptem@users.noreply.github.com>
2026-04-07 11:23:21 -0400

    repackage extras

--- sys/steam.nix
+++ sys/steam.nix
@@ -2,30 +2,29 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    gamescope
-    mangohud
-  ];
-
-  # Steam requires system-level installation for reasons
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
 
-    gamescopeSession = {
-      enable = true;
+    extraPackages = with pkgs; [
+      gamemode
+      mangohud
+      libkrb5
+      keyutils
+    ];
 
-      args = [ "mangoapp" ];
-    };
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
 
-    package = pkgs.steam.override {
-      extraPkgs =
-        pkgs: with pkgs; [
-          libkrb5
-          keyutils
-        ];
-    };
+    gamescopeSession.enable = true;
+  };
+
+  programs.gamemode.enable = true;
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = false;
   };
 }
